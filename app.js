ef1b046b6c8418ab9c277f7bc42a2b1e6152fff9
pedrojperez1 <pedrojperez1@gmail.com>
2024-04-10 11:07:59 -0400

    add listening message

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 var xhub = require('express-x-hub');
 
 app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
+app.listen(app.get('port'), 'Listening on port ' + app.get('port') );
 
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
