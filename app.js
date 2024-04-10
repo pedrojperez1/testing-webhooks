6988230d29fd72d8c687ae760eca3a7f2941874d
pedrojperez1 <pedrojperez1@gmail.com>
2024-04-10 10:39:45 -0400

    handle fb and ig webhooks

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const APP_NAME = 'TESTING WEBHOOKS';
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var xhub = require('express-x-hub');
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var token = process.env.TOKEN || 'token';
+var received_updates = [];
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
-app.post('/webhooks', (req, res) => {
-    console.log('Received webhook request:');
-    console.log(req.body);
-    res.sendStatus(200);
+app.get(['/facebook', '/instagram'], function(req, res) {
+  if (
+    req.query['hub.mode'] == 'subscribe' &&
+    req.query['hub.verify_token'] == token
+  ) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
-app.listen(PORT, () => {
-    console.log('Running app ' + APP_NAME + ' on port ' + PORT + '...');
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:', req.body);
+
+  if (!req.isXHubValid()) {
+    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    res.sendStatus(401);
+    return;
+  }
+
+  console.log('request header X-Hub-Signature validated');
+  // Process the Facebook updates here
+  console.log('Received FB update!');
+  console.log(req.body);
+  res.sendStatus(200);
 });
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  if (!req.isXHubValid()) {
+    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    res.sendStatus(401);
+    return;
+  }
+  // Process the Instagram updates here
+  console.log('Received FB update!');
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen();
