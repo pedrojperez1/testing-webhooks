bbb6921f9fc2a4f466f64c6d9cea5df55bacca0a
pedrojperez1 <pedrojperez1@gmail.com>
2024-04-10 11:21:31 -0400

    change listening port

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ var express = require('express');
 var app = express();
 var xhub = require('express-x-hub');
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
-
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
@@ -65,5 +62,5 @@ app.post('/instagram', function(req, res) {
   console.log(req.body);
   res.sendStatus(200);
 });
-
-app.listen();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, 'Listening on port ' + PORT + '...');
